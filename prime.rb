0383610bbfd81d29064eee4f4e3113aef93fcb66
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 22:27:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+  if integer == 1
+    return false
+  elsif integer > 0
+    (2..(integer - 1)).each do |n|
+      return false if integer % n == 0
+      end
+  else
+    (-2..(integer + 1)).each do |n|
+      return false if integer % n == 0
+      end
+    end
+  true
+end
