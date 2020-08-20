25fa8c23f8cadbe1c05e2ebbd7c1d0cc0d5b2994
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 22:14:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(integer)
+  if integer > 0
   (2..(integer - 1)).each do |n|
     return false if integer % n == 0
+  else 
+    (-2..(integer + 1)).each do |n|
+      return false if integer % n == 0
   end
   true
 end
